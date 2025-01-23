50ed76001c559084438b35ef1484f3406821bfa4
vitprofessional <46179916+vitprofessional@users.noreply.github.com>
2025-01-23 18:02:56 +0600

    update

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\AccountList;
+
+class FrontController extends Controller
+{
+    public function accountCreation(){
+        return view('adminPanel.accountCreation');
+    }
+    public function aclist(){
+        return view('adminPanel.accountList');
+    }
+
+    public function saveAccount(Request $requ){
+        $chk = AccountList::where(['acNumber'=>$requ->acNo])->get();
+        if(!$chk->isEmpty()):
+            return back()->with('error','Sorry! User already exist');
+        endif;
+
+        $data = new AccountList();
+
+        $data->acName = $requ->acName;
+        $data->acNumber = $requ->acNo;
+        $data->acType = $requ->acType;
+        $data->acMobile = $requ->acMobile;
+        $data->acFinger = $requ->acFinger;
+
+        if($data->save()):
+            return back()->with('success','Success! Account creation successfully');
+        else:
+            return back()->with('error','Opps! Account creation failed. Please try later');
+        endif;
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/account-create',[
+    FrontController::class,
+    'accountCreation'
+])->name('accountCreation');
+
+Route::post('/save-account',[
+    FrontController::class,
+    'saveAccount'
+])->name('saveAccount');
+
+Route::get('/account/list',[
+    FrontController::class,
+    'acList'
+])->name('acList');
+
+Route::get('/account/view/{id}',[
+    FrontController::class,
+    'acView'
+])->name('acView');
+
+Route::get('/account/edit/{id}',[
+    FrontController::class,
+    'acEdit'
+])->name('acEdit');
+
+Route::get('/account/del/{id}',[
+    FrontController::class,
+    'acDelete'
+])->name('acDelete');
